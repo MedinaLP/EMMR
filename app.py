4e6756ee92b4474e825e910f396d380bbb4666c5
MedinaLP <medinalp@students.national-u.edu.ph>
2025-06-05 23:39:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,13 @@ model = load_model()
 df.fillna(0, inplace=True)
 df.drop_duplicates(inplace=True)
 df['Country'] = df['Country'].str.strip()
+df.columns = df.columns.str.strip()  # Remove whitespace from column names
+
+# Rename if necessary
+if 'continent' in df.columns:
+    df.rename(columns={'continent': 'Continent'}, inplace=True)
+elif 'CONTINENT' in df.columns:
+    df.rename(columns={'CONTINENT': 'Continent'}, inplace=True)
 
 le = LabelEncoder()
 df['Country_Num'] = le.fit_transform(df['Country'])
